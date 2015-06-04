08c720da99f1ffe9b46ea0d6f6ac3d5e59e3ed42
Mayur Halai <halai_mayur@yahoo.com>
2015-06-05 00:12:48 +0100

    no message

---
+++ expanse.sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 05, 2015 at 01:11 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `expanse`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+`admin_id` int(11) NOT NULL,
+  `username` varchar(11) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `username`, `password`) VALUES
+(1, 'admin', '7ce38bf6811a96afd3411188577b08ee');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_transaction`
+--
+
+CREATE TABLE IF NOT EXISTS `admin_transaction` (
+`trans_id` int(11) NOT NULL,
+  `amount` double NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `username` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `split`
+--
+
+CREATE TABLE IF NOT EXISTS `split` (
+  `trans_id` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE IF NOT EXISTS `transaction` (
+`trans_id` int(11) NOT NULL,
+  `date` varchar(15) NOT NULL,
+  `description` varchar(128) DEFAULT NULL,
+  `amount` double NOT NULL,
+  `username` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='All transaction';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`user_id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `session` varchar(32) DEFAULT NULL,
+  `balance` double NOT NULL,
+  `common` tinyint(1) NOT NULL,
+  `forgot` tinyint(1) NOT NULL,
+  `approved` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='UserData';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+ ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `admin_transaction`
+--
+ALTER TABLE `admin_transaction`
+ ADD PRIMARY KEY (`trans_id`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+ ADD PRIMARY KEY (`trans_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`username`), ADD UNIQUE KEY `id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `admin_transaction`
+--
+ALTER TABLE `admin_transaction`
+MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
