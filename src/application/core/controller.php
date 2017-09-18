80459df179e3ecea5ca1ebde076a44d81d3fe285
Mayur <halai_mayur@yahoo.com>
2017-09-18 21:27:55 +0100

    removed comments

--- src/application/core/controller.php
+++ src/application/core/controller.php
@@ -14,8 +14,6 @@ class Controller
     
     private function openDatabaseConnection()
     {
-        // $option = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-        // $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASS, $option);
         $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';port=3306';
         $options = array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
